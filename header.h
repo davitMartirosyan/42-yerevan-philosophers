f01b0fcb062fa6ff46158d9c1b0e05a42bb3d72a
davitmartirosyan <phpencoding@gmail.com>
2023-01-16 19:00:32 +0400

    Philo day-01

---
+++ header.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmartiro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/16 18:12:31 by dmartiro          #+#    #+#             */
+/*   Updated: 2023/01/16 18:55:40 by dmartiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+# define OPTIONAL_TRUE 0x06
+# define ARGUMENT_SUCCESS 0x05
+# define ARGUMENT_ERROR 0x0A
+
+
+#endif
---
+++ philo.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmartiro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/16 17:09:08 by dmartiro          #+#    #+#             */
+/*   Updated: 2023/01/16 18:55:52 by dmartiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.h"
+
+int main(int ac, char **av)
+{
+    
+}
+
+
+
+//av[0] = : ./a.out
+//av[1] = : number_of_philosopher
+//av[2] = : time_to_die
+//av[3] = : time_to_eat
+//av[4] = : time_to_sleep    
+//av[5] = : [number_of_times_each_philosopher_must_eat]
+
+/*
+Example:
+
+    [timestamp_in_ms] Philosopher {X} has taken a fork
+    [timestamp_in_ms] Philosopher {X} is eating
+    [timestamp_in_ms] Philosopher {X} is sleeping
+    [timestamp_in_ms] Philosopher {X} is thinking
+    [timestamp_in_ms] Philosopher {X} died
+
+*/
